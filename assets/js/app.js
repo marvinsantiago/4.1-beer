bd188d52e50a361667f5327c433ef60ef9130db9
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-21 07:22:11 -0500

    Able to pull data to html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,18 @@
-var contentEl = document.querySelector('.listings');
+(function() {
 
-function templateByQuery(query) {
-  var templateString = document.querySelector(query).innerHTML;
+  function templateByQuery(query) {
+    var templateString = document.querySelector(query).innerHTML;
+    return Handlebars.compile(templateString);
+  }
 
-  return Handlebars.compile(templateString);
-}
+  var contentEl = document.querySelector('.listings');
+  var template = templateByQuery('#beer-template');
 
-var template = templateByQuery('#beer-template');
+  var inventory = '';
 
-beerData.data.forEach(function(data) {
-  // document.querySelector('.listings').innerHTML += data;
-  console.log(data);
-});
+  beerData.data.forEach(function(data) {
+    inventory += template(data);
+  });
 
+  contentEl.innerHTML = inventory;
+})();
